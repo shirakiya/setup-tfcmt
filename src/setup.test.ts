63320990d41cf75ee40bbf74a430881d68403320
shirakiya <shirakiya.pv@gmail.com>
2025-09-08 01:16:55 +0900

    update test expectation

--- src/setup.test.ts
+++ src/setup.test.ts
@@ -70,7 +70,7 @@ describe("mapArch", () => {
 describe("specifyReleaseVersion", () => {
   test("select latest one if passed version is empty", () => {
     const got = specifyReleaseVersion("")
-    expect(got).toStrictEqual("v4.14.10")
+    expect(got).toStrictEqual("v4.14.11")
   })
 
   test("select a matched version if passed version is specified", () => {
